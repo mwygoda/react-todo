65ae31fd2e6a6c01de5c7cd6e3767582e453f53f
mwygoda <mwygoda_94@o2.pl>
2016-11-26 11:07:48 +0100

    Add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -71,5 +71,18 @@ describe('TodoAPI', () => {
     var filteredTodos = TodoAPI.filterTodos(todos, false, '');
     expect (filteredTodos.length).toBe(1);
   });
+  it('should return uncompleted todos first ', ()=> {
+    var filteredTodos=TodoAPI.filterTodos(todos,true,'');
+    expect(filteredTodos[0].completed).toBe(false);
+  });
+  it('should return only items contains searchText', () => {
+    var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+    expect (filteredTodos.length).toBe(2);
+  });
+  it('should return all items if searchText is empty', () => {
+    var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+    expect (filteredTodos.length).toBe(3);
+  });
+
   });
 });
