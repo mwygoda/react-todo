32ac24bcb20d2cfede5771560fdefdda2377cf8f
mwygoda <mwygoda_94@o2.pl>
2016-12-18 13:13:42 +0100

    ??

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
     './app/app.jsx'
   ],
   externals: {
